c07ced6f5b5b2fe44f73924f1625fabe8a15c2d5
DonRuan22 <ruan_donino@hotmail.com>
2022-05-26 17:40:12 -0300

    create train_model

--- train_model.py
+++ train_model.py
@@ -43,9 +43,9 @@ tx_class = (
 
 tx_classdb = pd.get_dummies(tx_class)
 
-#tx_class['NextPurchaseDayRange'] = 2
-#tx_class.loc[tx_class.NextPurchaseDay>20,'NextPurchaseDayRange'] = 1
-#tx_class.loc[tx_class.NextPurchaseDay>50,'NextPurchaseDayRange'] = 0
+tx_class['NextPurchaseDayRange'] = 2
+tx_class.loc[tx_class.NextPurchaseDay>20,'NextPurchaseDayRange'] = 1
+tx_class.loc[tx_class.NextPurchaseDay>50,'NextPurchaseDayRange'] = 0
     
 logger.info('dataframe head - {}'.format(tx_classdb.describe()))    
 logger.info('dataframe head - {}'.format(tx_classdb.NextPurchaseDayRange))  
